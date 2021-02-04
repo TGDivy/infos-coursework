89b6b51fec143e95bff21773da40d18bdb416ad1
Divy Bramhecha <s1885517@ed.ac.uk>
2021-02-04 03:21:37 +0000

    next bit

--- coursework/cmos-rtc.cpp
+++ coursework/cmos-rtc.cpp
@@ -8,8 +8,23 @@
  */
 #include <infos/drivers/timer/rtc.h>
 #include <infos/kernel/log.h>
+#include <infos/kernel/sched.h>
+#include <infos/kernel/thread.h>
+#include <infos/util/list.h>
+#include <infos/util/lock.h>
+#include <arch/x86/pio.h>
 
-using namespace infos::kernel;
+// #include <infos/mm/page-allocator.h>
+// #include <infos/mm/mm.h>
+// #include <infos/kernel/kernel.h>
+// #include <infos/util/math.h>
+#include <infos/util/printf.h>
+
+using namespace infos::arch::x86;
+
+// using namespace infos::kernel;
+// using namespace infos::mm;
+using namespace infos::util;
 using namespace infos::drivers;
 using namespace infos::drivers::timer;
 
@@ -29,7 +44,12 @@ public:
 	 */
 	void read_timepoint(RTCTimePoint& tp) override
 	{
-		syslog.messagef(LogLevel::DEBUG, "WOOOO HOOOOOOOOOOO");
+		UniqueIRQLock();
+
+		__outb(0x70, 3); // Activate offset 3
+		uint8_t v = __inb(0x71); // Read data	
+		
+		// syslog.messagef(LogLevel::DEBUG, "WOOOO HOOOOOOOOOOO %d",v);
 		tp.seconds = 1;
 		tp.minutes = 1; 
 		tp.hours=1; 
