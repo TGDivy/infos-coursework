6c506fcef33fda52bc2a1089649d40465e46c0a1
tgdivy <s1885517@ed.ac.uk>
2021-03-18 12:23:07 +0000

    random

--- coursework/buddy.cpp
+++ coursework/buddy.cpp
@@ -286,7 +286,7 @@ public:
 			syslog.messagef(LogLevel::DEBUG, "tell meee %d, %d, %d", per_block, pfn_slot, pfn_pgd);
 			while (*slot && !(pfn_pgd>=pfn_slot && pfn_pgd<(pfn_slot+per_block))) {
 				slot = &(*slot)->next_free;
-				pfn_slot = sys.mm().pgalloc().pgd_to_pfn(*slot);	
+				pfn_slot = sys.mm().pgalloc().pgd_to_pfn(*slot);
 				syslog.messagef(LogLevel::DEBUG, "tell meee %d, %d, %d", per_block, pfn_slot, pfn_pgd);
 			}
 			if(*slot!=NULL && order == 0){
@@ -300,11 +300,11 @@ public:
 			}
 			else if(*slot!=NULL) {
 				assert(*slot);
-				split_block(slot, order);
+				split_block(slot, order+1);
 				return helper_reserve(order-1, pgd);
 			}
 			else {
-				return helper_reserve(order+1, pgd);	
+				return helper_reserve(order+1, pgd);
 			}
 		}
 		else {
