5392b6d10a01bde4df4ef26b8c6d8c8f86902310
tgdivy <s1885517@ed.ac.uk>
2021-03-18 09:57:44 +0000

    init

--- coursework/buddy.cpp
+++ coursework/buddy.cpp
@@ -309,8 +309,7 @@ public:
 		// TODO: Initialise the free area linked list for the maximum order
 		// to initialise the allocation algorithm.
 
-		
-		not_implemented();
+		_free_areas[MAX_ORDER-1] = page_descriptors;
 	}
 
 	/**
