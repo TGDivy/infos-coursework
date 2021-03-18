24f60e30d4eeee74a0df9221a8b35a73309af21e
tgdivy <s1885517@ed.ac.uk>
2021-03-18 13:28:45 +0000

    ..

--- coursework/buddy.cpp
+++ coursework/buddy.cpp
@@ -161,14 +161,14 @@ private:
 
 		PageDescriptor *buddy_pointer = buddy_of(*block_pointer, source_order-1);
 
-		PageDescriptor **block = insert_block(*block_pointer, source_order-1);
-		PageDescriptor **buddy = insert_block(buddy_pointer, source_order-1);
+		insert_block(*block_pointer, source_order-1);
+		insert_block(buddy_pointer, source_order-1);
 
 		// remove_block(*block_pointer, source_order);
 
 		// dump_state();
 
-		return *block > *buddy ? *buddy : *block;
+		return *block_pointer > buddy_pointer ?  buddy_pointer : *block_pointer;
 	}
 	
 	/**
